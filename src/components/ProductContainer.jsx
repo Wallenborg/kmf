a6f2c53a058faeb388851740f584d858f29f1fdc
Niklas Wallenborg <info@niklaswallenborg.com>
2025-06-02 09:15:56 +0200

    add price in cart and product

--- src/components/ProductContainer.jsx
+++ src/components/ProductContainer.jsx
@@ -1,47 +1,4 @@
-// import { RiShoppingBasket2Fill } from 'react-icons/ri';
-// import products from '../data/products';
-// import './ProductContainer.css';
-
-// const ProductContainer = ({ id, onAddToCart }) => {
-//   const product = products.find((p) => p.id === id);
-
-//   if (!product) {
-//     return null;
-//   }
-
-//   return (
-//     <div className="product-container">
-
-//       <img
-//         src={product.image}
-//         alt={product.title}
-//         className="product-container__image"
-//         loading="lazy"
-//       />
-
-//       <div className="product-container__title">
-//         {product.title}
-//       </div>
-
-//       <button
-//   className="product-container__add-button"
-//   onClick={(e) => {
-//     onAddToCart(product.id);
-//     e.currentTarget.blur();
-//   }}
-//   aria-label={`Add ${product.title} to cart`}
-// >
-//         <RiShoppingBasket2Fill size={24} />
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default ProductContainer;
-
-// src/components/ProductContainer.jsx
-
-import React, { useState } from "react";
+import { useState } from "react";
 import { RiShoppingBasket2Fill } from "react-icons/ri";
 import products from "../data/products";
 import "./ProductContainer.css";
@@ -74,17 +31,23 @@ const ProductContainer = ({ id, onAddToCart }) => {
 
       <div className="product-container__title">{product.title}</div>
 
-      <div className="product-container__button-wrapper">
-        {showConfirmation && (
-          <span className="product-container__confirmation">Added to cart</span>
-        )}
-        <button
-          className="product-container__add-button"
-          onClick={handleAddClick}
-          aria-label={`Add ${product.title} to cart`}
-        >
-          <RiShoppingBasket2Fill size={24} />
-        </button>
+      <div className="product-container__bottom-row">
+        <div className="product-container__price">{product.price} SEK</div>
+
+        <div className="product-container__button-wrapper">
+          {showConfirmation && (
+            <span className="product-container__confirmation">
+              Added to cart
+            </span>
+          )}
+          <button
+            className="product-container__add-button"
+            onClick={handleAddClick}
+            aria-label={`Add ${product.title} to cart`}
+          >
+            <RiShoppingBasket2Fill size={24} />
+          </button>
+        </div>
       </div>
     </div>
   );
